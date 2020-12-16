530e03883681b8692dad229e52975e46302eac37
TanayKapoor <itstkaay@gmail.com>
2020-12-16 10:12:51 +0530

    Final Card Animations

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const container = document.querySelector('.container');
 
 // Movement Animation 
 container.addEventListener('mousemove', (e) => {
-    let xAxis = window.innerWidth / 2 - e.pageX;
-    let yAxis = window.innerHeight / 2 - e.pageY;
-    card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`;
-
+    let xAxis = (window.innerWidth / 2 - e.pageX) /20;
+    let yAxis = (window.innerHeight / 2 - e.pageY) /20;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 })
+
+container.addEventListener('mouseenter', e =>{
+    card.style.transition = 'none';
+})
+
+ container.addEventListener('mouseleave', e => {
+
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
